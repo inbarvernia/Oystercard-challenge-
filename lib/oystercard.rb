33996b567c15a70c52db23f9d5af764630eb72dc
eatruman <lizzy_boucher@hotmail.com>
2021-03-08 15:47:50 +0000

    Challange 5, top up balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,12 @@
   class Oystercard
-    attr_reader :balance
+    attr_accessor :balance
     DEFAULT_BALANCE = 0
 
     def initialize(balance = DEFAULT_BALANCE)
       @balance = balance
     end
 
+    def top_up(amount)
+      @balance += amount
+    end
   end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,8 @@ describe Oystercard do
   it 'creates a new card with a balance of 0' do
     expect(card.balance).to eq(0)
   end
+  it 'tops up the balance of the card' do
+    allow(card).to receive(:balance) { 0 }
+    expect(card.top_up(5)).to eq(5)
+  end
 end

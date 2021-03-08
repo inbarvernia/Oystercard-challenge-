2ae981b1c4ac76d7232ddbd5782d48de110f5451
eatruman <lizzy_boucher@hotmail.com>
2021-03-08 16:21:03 +0000

    Challenge 6, maximum balance added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,15 @@
-  class Oystercard
-    attr_accessor :balance
-    DEFAULT_BALANCE = 0
+class Oystercard
+  attr_accessor :balance
+  DEFAULT_BALANCE = 0
+  CARD_LIMIT = 90
 
-    def initialize(balance = DEFAULT_BALANCE)
-      @balance = balance
-    end
-
-    def top_up(amount)
-      @balance += amount
-    end
+  def initialize
+    @balance = DEFAULT_BALANCE
   end
+
+  def top_up(amount)
+    @balance += amount
+    raise 'Maximum balance reached' if @balance > CARD_LIMIT
+    @balance
+  end
+end

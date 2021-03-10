d3676dde3c540a7e33b46621b88fda38317dd507
eatruman <lizzy_boucher@hotmail.com>
2021-03-10 09:16:09 +0000

    Deduct is now private and tests pass

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -22,14 +22,14 @@ describe Oystercard do
     end
   end
   # challenge 7
-  describe '#deduct()' do
-    context "when you tap out"
-      it 'deducts an amount from the balance' do
-      subject.top_up(20)
-      expect{ subject.deduct 3}.to change{ subject.balance }.by -3
-
-    end
-  end
+  # describe '#deduct()' do
+  #   context "when you tap out"
+  #     it 'deducts an amount from the balance' do
+  #     subject.top_up(20)
+  #     expect{ subject.deduct 3}.to change{ subject.balance }.by -3
+  #
+  #   end
+  # end
 
   #challenge 8
   describe '#in_journey?' do
